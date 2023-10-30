fa73e4f49f054a0acb38281b2091ee3989f2585a
narangrg443 <narangrg48@gmail.com>
2023-10-30 15:07:05 +0545

    navigate one screen to other

--- App.js
+++ App.js
@@ -1,111 +1,50 @@
-import React,{useState} from 'react';
-import { View, Text, FlatList,Button } from 'react-native';
-import { styles } from './style/AppStyle';
-import Checkbox from 'expo-checkbox';
-import questions from './components/Questions';
+// In App.js in a new project
+
+import * as React from 'react';
+import { View, Text,Button } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
 
 
-
-export default function App() {
-  const [questionNo,setQuestionNo]=useState(0);
-  const [goNext,setGoNext]=useState(false);
-  const [score,setScore]=useState(0);
-  const [answerColor,setAnswerColor]=useState(['blue','blue','blue','blue'])
-  
-  
-  const renderItem = ({ item,index}) => (
-    <View style={styles.answerOption}>
-      <Button style={styles.answerText} title={item}
-   onPress={()=>handleAnswer(index)}
-     color={answerColor[index]}
-
-      />
-    </View>
-  );
-  
-  //next question handler
-const nextQuestion=()=>{
-  if(goNext){
-    if(questionNo>=questions.length-1){setScore(0)}
-  setQuestionNo(prev=>prev>questions.length-2?0:prev+1);
-  setGoNext(false);
-  setAnswerColor(prev=>prev.map(data=>'blue'));
-}
-  
-}
-// answer handleer
- const handleAnswer=(id)=>{
-   //if answer is correct
-   
-   if(id===questions[questionNo].answer){
-     
-     if(!goNext){setScore(prev=>prev+1);}
-     setAnswerColor(prev=>{
-      const updatedColor=[...prev]
-      updatedColor[id]='green';
-      return updatedColor;
-      
-      });
-    }else{
-      //if answer is incorrect
-      setAnswerColor(prev=>{
-        const updatedColor=[...prev];
-        updatedColor[id]='red';
-        return updatedColor;
-      })
-    }  
-    
-    setGoNext(true);
- }
-
-
-
+function HomeScreen({ navigation }) {
   return (
-    
-    
-    <View style={styles.container}>
-    
-      <View style={styles.questionContainer}>
-    
-        
-        <Text style={{textAlign:'center',backgroundColor:'red'}}>questionNo:{`${score}/${questionNo+1}`}</Text>
-      
-        
-        <Text style={styles.question}>{questions[questionNo].question}</Text>
-        
-        
-      </View>
-         
-          <View style={styles.nextButton}>
-         {goNext?(
-        
-
-         <Button title="next" onPress={nextQuestion}/>
-         ):null
-         
-    }     
-    </View>
-      
-     
-      
-      
-      
-      <FlatList
-        data={questions[questionNo].options }
-        keyExtractor={(item,i) =>i.toString()}
-        renderItem={renderItem}
-        numColumns={2} // Set the number of columns as needed
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Home Screen</Text>
+      <Button
+        title="Go to Details"
+        onPress={() => navigation.navigate('Details',{name:'anil'})}
       />
-      <View style={styles.myButton} >
-  <Button title="reset" onPress={()=>{
-           //reset
-           setQuestionNo(0)
-           setScore(0)
-           setAnswerColor(prev=>prev.map(color=>'blue'));
-         }}
-         />
-         </View>
     </View>
   );
 }
+
+function DetailsScreen({ navigation }) {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Details Screen</Text>
+      <Button
+        title="Go to Details... again"
+        onPress={() => navigation.push('Details')}
+      />
+      <Button title="Go to Home" onPress={() => navigation.navigate('Home')} />
+      <Button title="Go back" onPress={() => navigation.goBack()} />
+    </View>
+  );
+}
+
+const Stack = createNativeStackNavigator();
+
+function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Home">
+        <Stack.Screen name="Home" component={HomeScreen} options={{title:'Overview'}} />
+        
+        
+        <Stack.Screen name="Details" component={DetailsScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+export default App;
--- style/AppStyle.js
+++ style/AppStyle.js
@@ -2,60 +2,12 @@ import { StyleSheet } from "react-native";
 
 export const styles = StyleSheet.create({
   container: {
-    flex:2/3,
+    flex:1,
     justifyContent: 'center',
     alignItems: 'center',
     backgroundColor: '#dfa851',
     
   },
-  questionContainer:{
-  
-  marginTop:50,
-  margin:10,
-  borderWidth:4,
-   borderColor:'black',
-    borderRadius:10,
-  },
-  question:{
-      width:350,
-    height:100,
-    borderRadius:5,
-    padding:10,
-    backgroundColor:'grey',
-    fontSize:18,
-   textAlign:'center',
-   textAlignVertical:'center',
-  
-  },
-answerOption:{
- width:150,
-  borderWidth:2.5,
-  borderRadius:5,
-  borderColor:'black',
-  margin:5,
-},
-answerText:{
-  fontSize:10,
-  width:100,
-  height:30,
-  backgroundColor:'green',
-  borderRadius:5,
-   textAlign:'center',
-   textAlignVertical:'center',
 
-  
-},
-nextButton:{
-  width:200,
-  height:40,
- 
-   
-  
-},
-myButton:{
- flex:10,
- 
-
-}
 
 });
