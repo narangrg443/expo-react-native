d968872b54b5416c1abaa5aeda5121d9d7437e74
narangrg443 <narangrg48@gmail.com>
2023-11-11 07:03:08 +0545

    created seperate checkbox

--- App.js
+++ App.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import { View, Text, Pressable } from 'react-native';
 import styles from './style/AppStyle';
-
+import Checkbox from './components/Checkbox';
 export default function App() {
   const [isChecked, setIsChecked] = useState(false);
 
@@ -11,75 +11,14 @@ export default function App() {
 
   return (
     <View style={styles.container}>
-      <CheckBoxRadio 
+      <Checkbox
       onPress={handlePress} 
       color2={'red'} 
-      type='circle'
+      type={'circle'}
       size={30}
-      
-      
       />
 
       {isChecked ? <Text>Checked</Text> : <Text>Not Checked</Text>}
     </View>
-  );
-}
-
-export function CheckBoxRadio({ onPress, type, color1, color2,size }) {
-  const [isChecked, setIsChecked] = useState(false);
-if(!size){
-  size =20;
-}
-  if (!type) {
-    type = 'box';
-  }
-
-  if (!color1) {
-    color1 = 'grey';
-  }
-
-  if (!color2) {
-    color2 = 'green';
-  }
-
-  const handlePress = () => {
-    setIsChecked((prev) => !prev);
-    onPress();
-  };
-
-  return (
-    <Pressable onPress={handlePress}>
-     <View style={[
-       {
-         justifyContent:'center',
-         alignItems:'center',
-         padding:2,
-        width:size,
-        height:size,
-        borderColor:color1,
-        borderWidth:1,
-        
-       },
-       isChecked&&{borderColor:color2},
-       type==='circle'&&{borderRadius:size/2},
-     ]}>
-      
-      
-      
-      {
-        isChecked&&(
-          <View style={[
-              {
-              width:size-6,
-              height:size-6,
-              backgroundColor:color2,
-              },
-              type==='circle'&&{borderRadius:size/2},
-            ]}>
-          </View>
-        )
-      }
-      </View>
-    </Pressable>
   );
 }
--- components/Checkbox.jsx
+++ components/Checkbox.jsx
@@ -2,85 +2,61 @@ import React,{useState} from 'react';
 import {View,Pressable,Text}from 'react-native';
 
 
-export default function CheckBoxRadio({onPress,type,color1,color2}){
-  const[isChecked,setIsChecked]=useState(false);
-  if(!type){
-    type='circle';
+export default function Checkbox({ onPress, type, color1, color2,size }) {
+  const [isChecked, setIsChecked] = useState(false);
+if(!size){
+  size =20;
+}
+  if (!type) {
+    type = 'box';
   }
-  if(!color1){
-    color1==='grey'
-    
+
+  if (!color1) {
+    color1 = 'grey';
   }
-  if(!color2){
-    color2==='grey';
+
+  if (!color2) {
+    color2 = 'green';
   }
-  
-    function onClicked(){
-    setIsChecked(prev=>!prev);
+
+  const handlePress = () => {
+    setIsChecked((prev) => !prev);
     onPress();
-  }
-  return(
-    <View>
-    
-    <Pressable 
-    onPress={onClicked}
-    >
-    <View style={[type==='circle'&&{
-      padding:1,
-      justifyContent:'center',
-      alignItems:'center',
-      width:20,
-      height:20,
-      borderRadius:10,
-      borderWidth:1,
-      borderColor:{color1}
-      
-    },type==='box'&&{
-      padding:1,
-      justifyContent:'center',
-      alignItems:'center',
-      width:20,
-      height:20,
-      borderWidth:1,
-      borderColor:{color1}]}>
-    
+  };
+
+  return (
+    <Pressable onPress={handlePress}>
+     <View style={[
        {
-      isChecked &&(
-    
-      <View style=
-      {[type==='circle'&&{
-        width:15,
-        height:15,
-        borderRadius:7.5,
-        borderWidth:2,
-        backgroundColor:{color2}
+         justifyContent:'center',
+         alignItems:'center',
+         padding:2,
+        width:size,
+        height:size,
+        borderColor:color1,
+        borderWidth:1,
         
-      },type==='box'&&{
-        width:14,
-        height:14,
-        backgroundColor:{color2}
-        
-      }]}>
+       },
+       isChecked&&{borderColor:color2},
+       type==='circle'&&{borderRadius:size/2},
+     ]}>
+      
+      
+      
+      {
+        isChecked&&(
+          <View style={[
+              {
+              width:size-6,
+              height:size-6,
+              backgroundColor:color2,
+              },
+              type==='circle'&&{borderRadius:size/2},
+            ]}>
+          </View>
+        )
+      }
       </View>
-      )
-    
-      
-      
-      
-    }
-    
-    
-    
-    </View>
-    
- 
-    
-    
-    
     </Pressable>
-    
-    </View>
-    
-    )
-  
+  );
 }
