4dbe91d4b1907ad146d3075d42c3a64acc893c6e
narangrg443 <narangrg48@gmail.com>
2023-11-14 11:10:02 +0545

    react-redux add and subtract

--- App.js
+++ App.js
@@ -1,62 +1,14 @@
-import React, { useState, useEffect } from 'react';
-import { View, Text, TextInput, Button, FlatList } from 'react-native';
-import AsyncStorage from '@react-native-async-storage/async-storage';
+import React from 'react';
+import { Provider } from 'react-redux'; // Assuming you are using react-redux for Redux integration
+import Counter from './components/Counter';
+import store from './context/store';
 
-export default function App() {
-  const [name, setName] = useState('');
-  const [nameArray, setNameArray] = useState([]);
-
-  useEffect(() => {
-    // Load data from AsyncStorage on component mount
-    loadData();
-  }, []);
-
-  const saveData = async () => {
-    try {
-      // Save data to AsyncStorage
-      const updatedArray = [...nameArray, name];
-      await AsyncStorage.setItem('nameArray', JSON.stringify(updatedArray));
-      console.log('Data saved successfully');
-      setNameArray(updatedArray);
-      setName('');
-    } catch (error) {
-      console.error('Error saving data:', error);
-    }
-  };
-
-  const loadData = async () => {
-    try {
-      // Load data from AsyncStorage
-      const storedData = await AsyncStorage.getItem('nameArray');
-      if (storedData !== null) {
-        const parsedData = JSON.parse(storedData);
-        setNameArray(parsedData);
-        console.log('Data loaded successfully');
-      }
-    } catch (error) {
-      console.error('Error loading data:', error);
-    }
-  };
-
-  const renderNameItem = ({ item }) => (
-    <Text style={{ marginVertical: 5 }}>{item}</Text>
-  );
-
+const App = () => {
   return (
-    <View style={{ margin: 20 }}>
-      <TextInput
-        style={{ backgroundColor: 'skyblue', height: 40, marginBottom: 10 }}
-        value={name}
-        placeholder="Enter name"
-        onChangeText={(text) => setName(text)}
-      />
-      <Button title="Save" onPress={saveData} />
-      <Button title="Show Data" onPress={loadData} />
-      <FlatList
-        data={nameArray}
-        renderItem={renderNameItem}
-        keyExtractor={(item, index) => index.toString()}
-      />
-    </View>
+    <Provider store={store}>
+      <Counter />
+    </Provider>
   );
-}
+};
+
+export default App;
---
+++ context/store.js
@@ -0,0 +1,6 @@
+// store.js
+import { createStore } from 'redux';
+import myReducer from './reducers';
+
+const store=createStore(myReducer);
+export default store;
